bfde89a5dddadaa02de77bdd5507ad7640a22597
yoav-ro <89576513+yoav-ro@users.noreply.github.com>
2021-12-22 22:57:46 +0200

    deployment

--- weather-app/src/apiRequests.js
+++ weather-app/src/apiRequests.js
@@ -1,9 +1,9 @@
-require('dotenv').config({ path: "../.env" })
+// require('dotenv').config({ path: "../.env" })
 const axios = require("axios")
-const apiKey = process.env.WEATHER_KEY;
-// const apiKey="1ae82b3204071ebdcedd698e8f77189e";
+// const apiKey = process.env.WEATHER_KEY;
+const apiKey="1ae82b3204071ebdcedd698e8f77189e";
 
-async function getWeather(city) {
+export async function getWeather(city) {
     try {
         const req = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
         return req;
@@ -11,9 +11,4 @@ async function getWeather(city) {
     catch(err){
         throw err.response.data;
     }
-}
-
-
-module.exports={
-    getWeather
 }
